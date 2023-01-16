e6e7bb0aee3d9ca3515d62772ee7bebaae114c90
choihs0457 <85881597+choihs0457@users.noreply.github.com>
2023-01-16 10:11:32 +0900

    [Bronze III] Title: 네 번째 점, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.py
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -1,25 +1,13 @@
-import sys
-
-input = sys.stdin.readline
-
-point_x = set()
-point_y = set()
-pointer_x = [0 for _ in range(1001)]
-pointer_y = [0 for _ in range(1001)]
-
-
+x_nums = []
+y_nums = []
 for _ in range(3):
-    N, M = map(int,input().split())
-    point_x.add(N)
-    point_y.add(M)
-    pointer_x[N] += 1
-    pointer_y[M] += 1
+    x, y = map(int, input().split())
+    x_nums.append(x)
+    y_nums.append(y)
 
-for x in point_x:
-    if pointer_x[x] == 1:
-        print(x, end=" ")
-        break
-for y in point_y:
-    if pointer_y[y] == 1:
-        print(y)
-        break
+for i in range(3):
+    if x_nums.count(x_nums[i]) == 1:
+        x4 = x_nums[i]
+    if y_nums.count(y_nums[i]) == 1:
+        y4 = y_nums[i]
+print(x4, y4)
