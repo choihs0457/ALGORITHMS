ffe88c89547033a3ed4495a2e62ca20b76ac2ae6
choihs0457 <85881597+choihs0457@users.noreply.github.com>
2023-01-19 11:55:55 +0900

    [unrated] Title: 롤케이크 자르기, Time: 471.40 ms, Memory: 51.3 MB -BaekjoonHub

--- 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,18 +1,17 @@
+from collections import Counter
+
 def solution(topping):
-    cnt = 0
-    ans_list = [0 for _ in range(len(topping))]
-    ans_list2 = [0 for _ in range(len(topping))]
-    first_list = set()
-    second_list = set()
+    answer = 0
+    dic = Counter(topping)
+    set_dic = set()
+    answer = 0
 
-    for x in range(len(topping)):
-        first_list.add(topping[x])
-        ans_list[x] = len(first_list)
-    for y in range(-1,-len(topping)-1,-1):
-        second_list.add(topping[y])
-        ans_list2[y] = len(second_list)
-    for z in range(len(topping)-1):
-        if ans_list[z] == ans_list2[z+1]:
-            cnt += 1
+    for i in topping:
+        dic[i] -= 1
+        set_dic.add(i)
+        if dic[i] == 0:
+            dic.pop(i)
+        if len(dic) == len(set_dic):
+            answer += 1
 
-    return(cnt)
+    return answer
